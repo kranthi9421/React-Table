1e2afa35d12486a799c195f5df9cf99043a0337d
kranthi <kranthi.007007@gmail.com>
2022-03-10 14:46:38 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+// import { BasicTable } from './components/BasicTable'
+import {SortingTable} from './components/SortingTable'
+
+function App() {
+  return (
+    <div className='App'>
+      {/* <BasicTable /> */}
+       <SortingTable/>
+    </div>
+  )
+}
+
+export default App
